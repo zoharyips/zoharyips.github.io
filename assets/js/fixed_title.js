cb612362142cee3408106138834e69a170eed70e
zoharyips <zoharyips@outlook.com>
2019-08-28 10:02:50 +0800

    fix test

--- assets/js/fixed_title.js
+++ assets/js/fixed_title.js
@@ -1,5 +1,23 @@
 window.onload = function(){
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+  var os = function (){
+    var ua = navigator.userAgent,
+    isWindowsPhone = /(?:Windows Phone)/.test(ua),
+    isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+    isAndroid = /(?:Android)/.test(ua),
+    isFireFox = /(?:Firefox)/.test(ua),
+    isChrome = /(?:Chrome|CriOS)/.test(ua),
+    isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox && /(?:Tablet)/.test(ua)),
+    isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+    isPc = !isPhone && !isAndroid && !isSymbian;
+    return {
+      isTablet: isTablet,
+      isPhone: isPhone,
+      isAndroid: isAndroid,
+      isPc: isPc
+    };	
+  }();
+  if (os.isAndroid || os.isPhone) {   
+    //alert("手机" );
     titleFixed();
   }
 }
