8d19415294f4e2c39655f35c6eeea38a2d36aa96
zoharyips <zoharyips@outlook.com>
2019-08-29 12:03:05 +0800

    add back to title

--- assets/js/fixed_title.js
+++ assets/js/fixed_title.js
@@ -41,3 +41,22 @@ function changePos(height){
     fixedTitle.style.position = 'none';
   }
 }
+
+var timer  = null;
+function titleBackToTop(){
+    cancelAnimationFrame(timer);
+    //获取当前毫秒数
+    var startTime = +new Date();     
+    //获取当前页面的滚动高度
+    var b = document.body.scrollTop || document.documentElement.scrollTop;
+    var d = 500;
+    var c = b;
+    timer = requestAnimationFrame(function func(){
+        var t = d - Math.max(0,startTime - (+new Date()) + d);
+        document.documentElement.scrollTop = document.body.scrollTop = t * (-c) / d + b;
+        timer = requestAnimationFrame(func);
+        if(t == d){
+          cancelAnimationFrame(timer);
+        }
+    });
+}
